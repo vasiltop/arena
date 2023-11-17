3b5e2f2c5b39c5a7b54dfe19e59600a028801145
Vasil <vasiltopalovic@outlook.com>
2023-11-17 09:52:20 -0500

    feat: sending generalized packet enum

--- server/src/main.rs
+++ server/src/main.rs
@@ -6,11 +6,10 @@ use std::io;
 use tokio::net::TcpListener;
 
 #[derive(Deserialize, Serialize, Debug)]
-struct Position {
-    #[serde(rename = "type")]
-    t: String,
-    x: i32,
-    y: i32,
+#[serde(tag = "type", rename_all = "snake_case")]
+
+enum Packet {
+    Position { x: i32, y: i32 },
 }
 
 #[tokio::main]
@@ -24,9 +23,12 @@ async fn main() -> io::Result<()> {
     }
 }
 
-async fn send_buf(buf: Vec<u8>, socket: &tokio::net::TcpStream) -> io::Result<()> {
-    socket.writable().await?;
+async fn send_buf(packet: Packet, socket: &tokio::net::TcpStream) -> io::Result<()> {
+    let mut buf = Vec::new();
+    packet.serialize(&mut Serializer::new(&mut buf)).unwrap();
+    buf.splice(0..0, buf.len().to_le_bytes());
 
+    socket.writable().await?;
     socket.try_write(buf.as_slice())?;
 
     Ok(())
@@ -41,22 +43,17 @@ async fn listen(socket: &tokio::net::TcpStream) -> io::Result<()> {
             Ok(n) => {
                 handle_packet(&buf, n);
 
-                let pos = Position {
-                    t: "Pos".to_string(),
-                    x: 1,
-                    y: 2,
-                };
+                let pos = Packet::Position { x: 1, y: 2 };
 
-                let mut buf = Vec::new();
-                pos.serialize(&mut Serializer::new(&mut buf)).unwrap();
-                buf.splice(0..0, buf.len().to_le_bytes());
-                send_buf(buf, socket).await?;
+                send_buf(pos, socket).await?;
             }
             Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
                 continue;
             }
+
             Err(e) => {
                 println!("Error reading from socket: {e}");
+                break Ok(());
             }
         }
     }
@@ -74,5 +71,5 @@ fn handle_packet(packet: &[u8; 1024], size: usize) {
 }
 
 fn handle_instruction(packet: &[u8]) {
-    println!("{:?}", rmp_serde::from_slice::<Position>(packet));
+    println!("{:?}", rmp_serde::from_slice::<Packet>(packet));
 }
